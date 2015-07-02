768b1cdc9417cae0ede47e5a54818304ce53f308
Hackbright Student <info@hackbrightacademy.com>
2015-07-02 12:59:09 -0700

    added check tokens length

--- calculator.py
+++ calculator.py
@@ -8,44 +8,58 @@ calculator program yourself in this file.
 from arithmetic import *
 
 
-# Your code goes here
 # while loop that's true
 while True:
     # take input and evaluate
     input = raw_input("Type a math equation! >")
     # split up values into a list
     tokens = input.split(" ") #tokens should be a list
-    # if q, a quit statement
-    if tokens[0] == 'q':
-        break
-    # run math equation according to list values
-    else:
-        # need to provide option for just one argument/input/integer
-        int1, int2 = int(tokens[1]), int(tokens[2])
-        if tokens[0] == "+":
-            result = add(int1, int2)
-            print result
-        elif tokens[0] == "-":
-            result = subtract(int1, int2)
-            print result
+    # tokens = ["*", "4", '3']
+    # check list length
 
-        elif tokens[0] == "*":
-            result = multiply(int1, int2)
-            print result
+        # if input length == 1
+            if len(tokens) == 1:
+                if tokens[0] == 'q':
+                    break
+                else:
+                    pass error()    
+        # if input length == 2
+            if len(tokens) == 2:
+                int1 = tokens[1]
+            # do int1
+                if tokens[0] == "square":
+                    print square(int1)
+                elif tokens[0] == "cube":
+                    print cube(int1) 
+                else:
+                    pass error()
+        # if input length == 3
+            if len(tokens) == 3:
+                int1 = tokens[1]
+                int2 = tokens[2]
+            # do int1, int2
+                if tokens[0] == "+":
+                    result = add(int1, int2)
+                    print result
 
-        elif tokens[0] == "/":
-            print divide(int1, int2)
+                elif tokens[0] == "-":
+                    result = subtract(int1, int2)
+                    print result
 
-        elif tokens[0] == "square":
-            print square(int1)
+                elif tokens[0] == "*":
+                    result = multiply(int1, int2)
+                    print result
 
-        elif tokens[0] == "cube":
-            print cube(int1) 
+                elif tokens[0] == "/":
+                    print divide(int1, int2)
 
-        elif tokens[0] == "pow":
-            print power(int1, int2)
+                elif tokens[0] == "pow":
+                    print power(int1, int2)
 
-        elif tokens[0] == "mod":
-            print mod(int1, int2)
+                elif tokens[0] == "mod":
+                    print mod(int1, int2)
+                else:
+                    pass error()
+
 
 
